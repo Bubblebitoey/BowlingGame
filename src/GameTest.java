40dd550f308f5d6871ec410543804d29745c8ea7
Soraya Saenna <bubblebitoey@gmail.com>
2018-02-01 23:41:55 +0700

    Perfect score donr and finish

--- src/GameTest.java
+++ src/GameTest.java
@@ -24,12 +24,16 @@ public class GameTest {
 		game.roll(1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1, 1,1);
 		assertThat(game.score(), is(20));
 	}
+	
 	@Test
 	public void canScoreStrikeFollowedByThreeThenThree() {
 		game.roll(10, 3,3, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0, 0,0);
 		assertThat(game.score(), is(22));
 	}
 	
-	
-	  
+	@Test
+	public void canScorePerfectGame() {
+		game.roll(10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10, 10);
+		assertThat(game.score(), is(300));
+	}
 }
