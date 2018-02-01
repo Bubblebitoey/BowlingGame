dce4713cdc186bde3596ccf7d7c6946ef1b741df
Soraya Saenna <bubblebitoey@gmail.com>
2018-02-01 22:59:35 +0700

    Create roll method in GmaeTest

--- src/GameTest.java
+++ src/GameTest.java
@@ -1,4 +1,3 @@
-
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
@@ -13,34 +12,26 @@ public class GameTest {
 	public void setUp() {
 		game = new Game();
 	}
-	
-//	@Test
-//	public void canMakeGame() {
-//		new Game();
-//	}
-	
-//	@Test
-//		public void canRollBall() {
-//			Game game = new Game();
-//			game.roll(0);
-//		}
 		
 	@Test
 	public void canScoreCutterGame() {
-//		Game game = new Game();
-		for (int i = 0; i < 20; i++) {
-			game.roll(0);
-		}
+		int times = 20;
+		int pinsDown = 0;
 		assertThat(game.score(), is(0));
 	}
 	
 	@Test
 		public void camScoreGameOfOnes() {
-//			Game game = new Game();
 			for (int i = 0; i < 20; i++) {
 				game.roll(1);
 			}
 			assertThat(game.score(), is(20));
 		}
+		
+	private void roll(int times, int pinsDown) {
+		for (int i = 0; i < times; i++) {
+			game.roll(pinsDown);
+		}
+	}
 	
 }
