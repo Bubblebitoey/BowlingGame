2e7ff05393c0124a419bbe34ea4adbde0d2a6c4a
Soraya Saenna <bubblebitoey@gmail.com>
2018-02-01 22:49:53 +0700

    Make Game as constructor in GaemTest

--- src/GameTest.java
+++ src/GameTest.java
@@ -2,9 +2,17 @@
 import org.junit.Test;
 import static org.junit.Assert.*;
 import static org.hamcrest.CoreMatchers.*;
+import org.junit.Before;
 
 
 public class GameTest {
+	
+	private Game game;
+	
+	@Before
+	public void setUp() {
+		game = new Game();
+	}
 	
 //	@Test
 //	public void canMakeGame() {
@@ -19,10 +27,20 @@ public class GameTest {
 		
 	@Test
 	public void canScoreCutterGame() {
-		Game game = new Game();
+//		Game game = new Game();
 		for (int i = 0; i < 20; i++) {
 			game.roll(0);
 		}
 		assertThat(game.score(), is(0));
 	}
+	
+	@Test
+		public void camScoreGameOfOnes() {
+//			Game game = new Game();
+			for (int i = 0; i < 20; i++) {
+				game.roll(1);
+			}
+			assertThat(game.score(), is(20));
+		}
+	
 }
